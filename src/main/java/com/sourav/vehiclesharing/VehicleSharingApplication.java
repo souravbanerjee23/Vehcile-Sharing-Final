868875f1f2b2deaa13b686eff0d4f48b7452f51b
Sourav Bandyopadhyay <souravbandyopadhyay1997@gmail.com>
2026-09-14 10:27:26 +0530

    Add Spring Boot application entry point

---
+++ src/main/java/com/sourav/vehiclesharing/VehicleSharingApplication.java
@@ -0,0 +1,11 @@
+package com.sourav.vehiclesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSharingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleSharingApplication.class, args);
+    }
+}
